5357134ecda1e1611a91ea7338a87de12168f517
Алексей Суриков <ksuri@cpan.org>
2011-06-04 15:45:47 +0000

    Import of KSURI/App-Mver-0.04 from CPAN.
    gitpan-cpan-distribution: App-Mver
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         KSURI/App-Mver-0.04.tar.gz
    gitpan-cpan-author:       KSURI
    gitpan-cpan-maturity:     released

--- lib/App/Mver.pm
+++ lib/App/Mver.pm
@@ -1,12 +1,78 @@
 package App::Mver;
 
-our $VERSION = '0.03';
+use strict;
+use warnings;
 
-# vim: ft=perl
+use Pod::Usage;
+use Pod::Find qw(pod_where);
+
+our $VERSION = '0.04';
+
+my $module_corelist = eval "use Module::CoreList; 1";
+
+sub run {
+    @_ or usage();
+    usage(2) if $_[0] eq '-h' or $_[0] eq '--help';
+
+    mver($_) for @_;
+}
+
+sub mver {
+    my $arg = shift;
+    $arg =~ s{-}{::}g;
+
+    my $is_loaded = eval "use $arg;1";
+    print "$arg: ";
+    unless(defined $is_loaded) {
+        if($@ =~ /^Can't locate/) {
+            print 'not installed';
+        }
+        else {
+            print 'installed, but contains error';
+        }
+    }
+    else {
+        my $version = $arg->VERSION;
+        if(defined $version) {
+            print $version;
+
+            my $authority = eval "\$$arg\::AUTHORITY";
+            if(defined $authority) {
+                print " ($authority)";
+            }
+
+            if($module_corelist and is_core($arg)) {
+                print ' (core module)';
+            }
+        }
+        else {
+            print 'installed, but $VERSION is not defined';
+        }
+    }
+    print $/;
+}
+
+sub is_core {
+    my $arg = shift;
+
+    my($found_in_core) = Module::CoreList->find_modules(qr/^\Q$arg\E$/, $]);
+
+    !!$found_in_core;
+}
+
+sub usage {
+    pod2usage(
+        -verbose => $_[0],
+        -input   => pod_where(
+            { -inc => 1 },
+            __PACKAGE__,
+        ),
+    );
+}
+
+1;
 
 __END__
-
-=encoding utf8
 
 =head1 NAME
 
@@ -20,13 +86,11 @@ For those, who are sick of
 
 =head1 SYNOPSIS
 
-    mver Module::Name
-
-    mver Module-Name
+    mver Module::Name1 Module-Name2 Module::NameN
 
 =head1 AUTHOR
 
-Алексей Суриков E<lt>ksuri@cpan.orgE<gt>
+Alexey Surikov E<lt>ksuri@cpan.orgE<gt>
 
 =head1 LICENSE
 
